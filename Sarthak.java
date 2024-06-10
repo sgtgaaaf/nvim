d25e2016980034c1cd846a1578b490f24482dfde
sgtgaaaf <sarthakghimire@gmail.com>
2024-06-10 19:00:59 +0100

    added few more plugins to the unix branch

---
+++ Sarthak.java
@@ -0,0 +1,8 @@
+public class Sarthak {
+   public static void main(String[] args) {
+      System.out.println("Hello world");
+      System.out.println("Hello");
+      System.out.println("Hello");
+      System.out.println("");
+   }
+}
